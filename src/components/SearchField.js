07453d1e1c3eff01323ebf182b4f7a382ff4fdf1
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-03-05 15:09:37 -0600

    Updated Searcd Component

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,29 +1,43 @@
-import { useContext } from 'react'
-import { BreweryContext } from '../context/BreweryContext'
+import { useContext } from 'react';
+import { BreweryContext } from '../context/BreweryContext';
 
- export const SearchField = () => {
-  const { searchValue, handleChange, handleSubmit } = useContext(BreweryContext)
-   return (
+const SearchField = () => {
+  const { searchValue, handleChange, handleSubmit } = useContext(BreweryContext);
+  return (
     <div className='relative flex justify-center items-center'>
-    <form onSubmit={handleSubmit} >
-      <div className='form-control '>
-        <div className='relative'>
-          <input
-            type='text'
-            className='w-full pr-40 bg-gray-200 input input-lg text-black text-xl'
-            placeholder='Search'
-            value={searchValue}
-            onChange={handleChange}
-          />
-          <button
-            type='submit'
-            className='absolute top-0 right-0 rounded-l-none w-36 btn btn-accent btn-lg'
-          >
-            Go
-          </button>
+      <form onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <div className='relative'>
+            <input
+              type='text'
+              className='w-full pr-40 bg-gray-200 input input-lg text-black text-xl'
+              placeholder='Search'
+              value={searchValue}
+              onChange={handleChange}
+              aria-label='Search breweries powered by OpenAI'
+            />
+            <button
+              type='submit'
+              className='absolute top-0 right-0 rounded-l-none w-36 btn btn-accent btn-lg'
+            >
+              Go
+            </button>
+          </div>
+          <span className='text-sm text-gray-400 mt-1'>
+            Search breweries powered by&nbsp;
+            <a
+              href='https://openai.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline'
+            >
+              OpenAI
+            </a>
+          </span>
         </div>
-      </div>
-    </form>
-  </div>
-   )
- }
+      </form>
+    </div>
+  );
+};
+
+export {SearchField};
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const About = () => {
   return (
     <div className='hero min-h-screen bg-base-200'>
@@ -5,7 +7,7 @@ const About = () => {
         <div className='max-w-md'>
           <h1 className='text-5xl font-bold'>Brewery Finder</h1>
           <p className='py-6'>
-          This awesome brewery-finding app is powered by the Open Brewery DB API and OpenAI. With just a few taps, you can discover new and exciting breweries to visit in the US and beyond, meet new people, and enjoy amazing brews. Download the app today and start exploring the wonderful world of beer! 🍻
+            This awesome brewery-finding app is powered by the Open Brewery DB API and OpenAI. With just a few taps, you can discover new and exciting breweries to visit in the US and beyond, meet new people, and enjoy amazing brews. Download the app today and start exploring the wonderful world of beer! 🍻
           </p>
         </div>
       </div>
