22334c9f06f3828dfddcfacc940a71ea93b98b9b
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-03-05 13:08:12 -0600

    Updated the about section.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className='max-w-md'>
           <h1 className='text-5xl font-bold'>Brewery Finder</h1>
           <p className='py-6'>
-          Looking for a way to discover new and exciting breweries to visit in the US and beyond? Look no further than our awesome app! Using the power of the <strong><a href="https://www.openbrewerydb.org/" target='_blank' rel="noreferrer">Open Brewery DB API</a></strong>, you can easily search for amazing breweries in your area or in far-off destinations. With just a few taps on your screen, you'll be on your way to discovering amazing brews, meeting cool new people, and having the time of your life. So why wait? Download our app today and start exploring the wonderful world of beer! Cheers! 🍻
+          This awesome brewery-finding app is powered by the Open Brewery DB API and OpenAI. With just a few taps, you can discover new and exciting breweries to visit in the US and beyond, meet new people, and enjoy amazing brews. Download the app today and start exploring the wonderful world of beer! 🍻
           </p>
         </div>
       </div>
