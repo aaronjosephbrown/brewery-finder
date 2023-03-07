593b9eb2f93a7cbd019d6dd145671d303115c6e2
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-03-06 20:31:10 -0600

    Updated training data

--- src/context/openai.js
+++ src/context/openai.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const handleListEngines = async (text) => {
   const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-    prompt: `Extract the city from the text:\n\nText: I want to have a beer in Minneapolis "${text}"\nCity: Minneapolis \n \nText: I am head toward Tampa, FL "${text}"\nCity: Tampa \nText: I want to have a beer in"${text}"\nCity: `,
+    prompt: `Extract the city from the text which could also have an airport code:\n
+    \nText: I want to have a beer in Minneapolis ${text}\nCity: Minneapolis\n
+    \nText: I am head toward Tampa, FL ${text}\nCity: Tampa \n
+    \nText: Beer at HNL ${text}\n City: Honolulu\n
+    \nText: Need a beer in MSP or msp ${text}\n City: Minneapolis\n
+    \nText: Flying to JAX or jax or Jax ${text}\n City: Jacksonville\n
+    \nText: I want to have a beer in ${text}\n City: `,
     temperature: 0,
     max_tokens: 60,
     top_p: 1.0,
